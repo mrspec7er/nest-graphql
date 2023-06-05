4659eb4e3b322aa9832bbc63f7f45f86d5eefd8b
Kusuma Sandi <wijayakusumasandi@gmail.com>
2023-06-05 06:37:10 +0000

    feat(organization) added auth guard to organization resolver

--- src/organization/organization.service.ts
+++ src/organization/organization.service.ts
@@ -1,9 +1,6 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { Organization } from './organization.model';
-import {
-  Organization as OrganizationType,
-  CreateOrganizationInput,
-} from '../graphql';
+import { Organization as OrganizationType } from '../graphql';
 import { UserService } from '../user/user.service';
 
 @Injectable()
@@ -32,11 +29,51 @@ export class OrganizationService {
 
     this.userService.updateOrganization({ id: organization.id, userId });
 
-    return {
-      name: organization.name,
-      created: String(organization.created),
-      updated: String(organization.updated),
-    };
+    let typeParser: any = organization;
+
+    return typeParser;
+  }
+
+  async update({
+    name,
+    id,
+  }: {
+    name: string;
+    id: string;
+  }): Promise<OrganizationType> {
+    const organization = await Organization.findOneAndUpdate(
+      { _id: id },
+      { name, updated: Date.now() },
+    );
+
+    let typeParser: any = organization;
+
+    return typeParser;
+  }
+
+  async updateMember({ id, userId }: { id: string; userId: string }) {
+    const organizationMember = (await Organization.findById(id))?.users;
+
+    if (Array.isArray(organizationMember)) {
+      await Organization.updateOne(
+        { _id: userId },
+        { $set: { users: [...organizationMember, userId] } },
+      );
+    }
+
+    return id;
+  }
+
+  async getMyOrganizationList({
+    userId,
+  }: {
+    userId: string;
+  }): Promise<OrganizationType[]> {
+    const organization = await Organization.find({ users: { $in: [userId] } });
+
+    let typeParser: any = organization;
+
+    return typeParser;
   }
 
   async updateMember({ id, userId }: { id: string; userId: string }) {
